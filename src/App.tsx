73fd4a54a13115f870aa4b9d87bf4e85ddfa9f97
MagicMarvel <MagicMarvel@163.com>
2022-11-19 20:49:45 +0800

    添加了可以用对象设置style

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,35 @@ import { useState } from "./WeiReact";
 // 注意：函数式组件的命名的首字母必须大写
 
 function Wei(props: any) {
-  const [num, setNum] = useState(1);
-  return (
-    <div>
-      <h1 className="hahaha">凯哥好厉害！</h1>
-      <h1 className="iiiiii" style="color:pink">
-        *\^_^/*
-      </h1>
-      <h1
-        onclick={() => {
-          setNum(2);
-        }}
-      >
-        {num}
-      </h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="hahaha">凯哥好厉害！</h1>
+            <h1 className="iiiiii" style={{ color: "blue", fontSize: "50px" }}>
+                *\^_^/*
+            </h1>
+        </div>
+    );
 }
 
 function Kai(props: any) {
-  return (
-    <div>
-      <h1 className="hahaha">薇薇也是~</h1>
-      <h1 className="iiiiii" style="color:red">
-        *\^3^/*
-      </h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="hahaha">薇薇也是~</h1>
+            <h1 className="iiiiii" style="color:red">
+                *\^3^/*
+            </h1>
+        </div>
+    );
 }
 
 function Com(props: any) {
-  return (
-    <div>
-      <Wei />
-      <div style="width:100vw;height:200px;background-color:#ddedfb;"></div>
-      <Kai />
-    </div>
-  );
+    return (
+        <div>
+            <Wei />
+            <div style="width:100vw;height:200px;background-color:#ddedfb;"></div>
+            <Kai />
+        </div>
+    );
 }
 
 WeiReact.render(<Wei />, document.getElementById("root"));
